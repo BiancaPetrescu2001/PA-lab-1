3fff867faff4ba06ae4c0c11fb99d4a5b29f18ae
BiancaPetrescu2001 <biancapetrescu2001@gmail.com>
2021-02-21 01:58:20 +0200

    STEP 1-4

--- src/main/java/compulsory/App.java
+++ src/main/java/compulsory/App.java
@@ -1,8 +1,35 @@
 package compulsory;
 
 public class App {
-    public static void main(String args[])
+    public static void PrintMessage()
     {
         System.out.println("Hello world!");
+    }
+    public static void DefineArray()
+    {
+        String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+    }
+    public static int GenerateInt()
+    {
+        int n = (int) (Math.random() * 1_000_000);
+        return n;
+    }
+    public static void Modify(int n)
+    {
+        n=n*3;
+        String binaryStr="10101";
+        n=n+Integer.parseInt(binaryStr,2);
+        String hexdecStr="FF";
+        n=n+Integer.parseInt(hexdecStr, 16);
+        n=n*6;
+    }
+    public static void main(String args[])
+    {
+        int n;
+        PrintMessage();
+        DefineArray();
+        n=GenerateInt();
+        Modify(n);
+        System.out.println(n);
     }
 }
